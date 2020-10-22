7bde5f8d0745d0576a53909c39cf962db346ab4f
Tim Holy <tim.holy@gmail.com>
2020-10-22 13:51:23 -0500

    Analyze prospective version bumps (#4)
    If you're thinking about bumping the version number of a package,
    sometimes it's nice to know in advance what you'll have to bump.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,5 @@ using Test
 
 @test held_back_packages() isa Dict
 @test held_back_by("Requires") isa Vector{String}
+# The following test assumes that at least one of the dependents of ColorTypes is compatible with the latest version
+@test length(held_back_by("ColorTypes", typemax(VersionNumber))) > length(held_back_by("ColorTypes"))
